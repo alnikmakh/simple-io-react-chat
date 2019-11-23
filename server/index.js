1ec203c5ba69fe46815f6b04788303c941c56471
alnikmakh <alnikmakh@gmail.com>
2019-11-23 14:49:15 +0300

    customize environment in server directory & add simple express server

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Listen on *: ${PORT}`));
