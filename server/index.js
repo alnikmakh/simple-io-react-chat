1cf0f61486aebf5f06cd255c51c6908a401ed1ae
alnikmakh <alnikmakh@gmail.com>
2019-11-23 15:05:07 +0300

    install socket.io-client & create simple connect for test

--- server/index.js
+++ server/index.js
@@ -4,4 +4,10 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
 const PORT = process.env.PORT || 5000;
+
 server.listen(PORT, () => console.log(`Listen on *: ${PORT}`));
+
+io.on("connection", socket => {
+  const id = socket.client.id;
+  console.log(`User connected: ${id}`);
+});
