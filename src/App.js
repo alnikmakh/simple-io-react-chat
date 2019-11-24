4f2c746148e4204d7d6e3daf63e1e31f6dae8126
alnikmakh <alnikmakh@gmail.com>
2019-11-24 14:26:38 +0300

    Add input & rendering nicknames

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import io from "socket.io-client";
 
 //const socket = io.connect("https://server-io-chat.herokuapp.com/");
 const socket = io.connect("http://localhost:5000");
+let nick;
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { msg: "", chat: [] };
+    this.state = { nick: "", msg: "", chat: [] };
   }
   // I dont use .bind for functions below because i use "arrow functions"
   componentDidMount() {
@@ -23,21 +24,31 @@ class App extends Component {
   onTextChange = e => {
     this.setState({ msg: e.target.value });
   };
+  
+  onNickChange = e => {
+    nick = e.target.value;
+  };
+  
+  onNickSubmit = () => {
+    this.setState({ nick: nick });
+  };
 
   // Function for sending message to chat server
   onMessageSubmit = () => {
-    const { msg } = this.state;
-    const id = socket.id;
-    socket.emit("chat message", { id, msg });
+    const response = {
+      msg: this.state.msg,
+      nick: nick
+    };
+    socket.emit("chat message", response);
     this.setState({ msg: "" });
   };
   
   renderChat() {
     const { chat }  = this.state;
     //idx of each element of chat, need for generate key for each JSX tag
-    return chat.map(({ id, msg }, idx) => (
+    return chat.map(({ nick, msg }, idx) => (
       <div key={idx}>
-        <span style={{ color: "green" }}>{id}: </span>
+        <span style={{ color: "green" }}>{nick}: </span>
         <span>{msg}</span>
       </div>
     ));
@@ -46,9 +57,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        <input onChange={e => this.onTextChange(e)} value={this.state.msg} />
-        <button onClick={this.onMessageSubmit}>Send</button>
-        <div>{this.renderChat()}</div>
+        <span>{this.state.nick ? this.state.nick : "Enter your nickname"}</span>
+        {this.state.nick ? null : <input onChange={e => this.onNickChange(e)} />}
+        {this.state.nick ? null : <button onClick={this.onNickSubmit}>Ok</button>}
+
+        {this.state.nick ? <input onChange={e => this.onTextChange(e)} value={this.state.msg} /> : null }
+        {this.state.nick ? <button onClick={this.onMessageSubmit}>Send</button> : null }
+        {this.state.nick ? <div>{this.renderChat()}</div> : null }
       </div>
     );
   }
